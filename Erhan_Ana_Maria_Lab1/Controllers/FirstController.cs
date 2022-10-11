ee079181ff2ec75c1e4d8c3393f881e18e614f31
Ana-Maria Erhan <Ana.Erhan@cloudflight.io>
2022-10-11 19:22:03 +0300

    Added FirstController

---
+++ Erhan_Ana_Maria_Lab1/Controllers/FirstController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Erhan_Ana_Maria_Lab1.Controllers
+{
+    public class FirstController : Controller
+    {
+        public String Index()
+        {
+            return "Welcome, user!";
+        }
+
+        public String HelloStudent(String studentName)
+        {
+            return "Hello, " + studentName;
+        }
+
+        public String GreetStudent(String studentName, int studentAge)
+        {
+            return "Student " + studentName + " has " + studentAge + " years old."; 
+        }
+    }
+}
